045b9bd9730a34ffeadbc8e157c0b7c4a428da36
Salman <soleimanpoursalman@gmail.com>
2026-08-02 12:03:42 +0330

    Temp: set hard redirect to /app directory for focus on super app

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,17 +1,27 @@
-import { HeroSection } from './sections/hero/HeroSection'
-import { EcosystemSection } from './sections/ecosystem/EcosystemSection'
-import { WhyDotOneSection } from './sections/why-dotone/WhyDotOneSection'
-import { ProductsSection } from './sections/products/ProductsSection'
-import { CTASection } from './sections/cta/CTASection'
+"use client";
+
+// import { HeroSection } from './sections/hero/HeroSection'
+// import { EcosystemSection } from './sections/ecosystem/EcosystemSection'
+// import { WhyDotOneSection } from './sections/why-dotone/WhyDotOneSection'
+// import { ProductsSection } from './sections/products/ProductsSection'
+// import { CTASection } from './sections/cta/CTASection'
+import { useRouter } from "next/navigation";
+import { useEffect, useLayoutEffect } from "react";
 
 export function HomePage() {
+  const navigate = useRouter();
+
+  useLayoutEffect(() => {
+    navigate.replace("/app");
+  }, []);
+
   return (
     <main id="main-content">
-      <HeroSection />
+      {/* <HeroSection />
       <WhyDotOneSection />
       <EcosystemSection />
       <ProductsSection />
-      <CTASection />
+      <CTASection /> */}
     </main>
-  )
+  );
 }
